d564374e56e78fac9a14b6dc11cbbbb4bd90c6db
Ahmad Hafizh Alkarim <hafizh.alkarim02@gmail.com>
2021-05-18 14:56:08 +0700

    Initial commit

---
+++ app/src/main/java/spirit/bangkit/kusaku/MainActivity.kt
@@ -0,0 +1,16 @@
+package spirit.bangkit.kusaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import spirit.bangkit.kusaku.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
